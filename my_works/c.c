98dd94e796b68bc8ab94b3e00f5ed5e462b06e2c
Sarthak <ytsarthak1@gmail.com>
2025-10-22 10:40:27 +0545

    lol

--- my_works/c.c
+++ my_works/c.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-#include <string.h>
-
-int main()
-{
-    char str[50] = "Hello World";
-    printf("Length: %lu\n", strlen(str));
-    printf("Copy: %s\n", strcpy(str, "Hi!"));
-    printf("Concat: %s\n", strcat(str, " How are you?"));
-    printf("Compare: %d\n", strcmp("abc", "abd"));
-    printf("Find char: %s\n", strchr(str, '!'));
-    printf("Find substring: %s\n", strstr(str, "How"));
-
-    return 0;
-}
+ 
