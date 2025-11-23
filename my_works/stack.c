0cd8a036c044ff0a3eadf6a2793f1f67c9f91700
Sarthak <ytsarthak1@gmail.com>
2025-11-23 15:35:50 +0545

    final chan

--- my_works/stack.c
+++ my_works/stack.c
@@ -1,66 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define Max 5
 
 int stack[Max];
 int top = -1;
 
-void push()
+void push(void)
 {
     int num;
 
     if (top == Max - 1)
     {
-        printf("The stack is full. \n");
+        printf("The stack is full.\n");
     }
     else
     {
         top++;
         printf("Enter any number: ");
-        scanf("%d", &num);
-        printf("Number %d has been added to stack: \n\n", num);
-        stack[top] = num;
-    }
-}
-
-void pop()
-{
-    if (top == -1)
-    {
-        printf("The stack is empty: \n ");
-    }
-    else
-    {
-        printf("Number %d is removed from stack: ", stack[top--]);
-    }
-}
-
-void display_all()
-{
-    if (top == -1) // indicates the stack is empty.
-    {
-        printf("The stack is empty , Hence , no display. \n");
-    }
-
-    else
-    {
-        for (int i = top; i >= 0; --i) // assuming top > -1.
+        if (scanf("%d", &num) == 1)
         {
-            printf("%d\t->", stack[i]);
+            stack[top] = num;
+            printf("Number %d has been added to the stack.\n\n", num);
         }
-        printf("\n");
+        else
+        {
+            /* consume invalid input */
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF)
+            {
+            }
+            printf("Invalid input. No number was added.\n\n");
+            top--;
+        }
     }
 }
 
-void display_top()
+void pop(void)
 {
     if (top == -1)
     {
-        printf("The stack is empty , Hence , The stack has no top value: \n");
+        printf("The stack is empty.\n");
     }
     else
     {
-        printf("The top of the stack value is: %d\n\n", stack[top]);
+        printf("Number %d is removed from the stack.\n", stack[top--]);
+    }
+}
+
+void display_all(void)
+{
+    if (top == -1)
+    {
+        printf("The stack is empty; nothing to display.\n");
+    }
+    else
+    {
+        for (int i = top; i >= 0; --i)
+        {
+            printf("--> %d\n", stack[i]);
+        }
+    }
+}
+
+void display_top(void)
+{
+    if (top == -1)
+    {
+        printf("The stack is empty; there is no top value.\n");
+    }
+    else
+    {
+        printf("The top of the stack is: %d\n\n", stack[top]);
     }
 }
 
@@ -68,15 +79,24 @@ int main(void)
 {
     while (1)
     {
-        printf("Enter one of the following: ");
-        printf("\n1. PUSH: ");
-        printf("\n2. POP: ");
-        printf("\n3. TRAVERSE: ");
-        printf("\n4. Display Top: ");
-        printf("\n5. Exit: \n");
+        printf("Enter one of the following:");
+        printf("\n1. PUSH");
+        printf("\n2. POP");
+        printf("\n3. TRAVERSE");
+        printf("\n4. Display Top");
+        printf("\n5. Exit\n");
+
         int choice;
-        printf("-->> : ");
-        scanf("%d", &choice);
+        printf("-->> ");
+        if (scanf("%d", &choice) != 1)
+        {
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF)
+            {
+            }
+            printf("Invalid choice. Please enter a number between 1 and 5.\n\n");
+            continue;
+        }
 
         switch (choice)
         {
@@ -93,12 +113,13 @@ int main(void)
             display_top();
             break;
         case 5:
-            printf("Exiting the program. ");
+            printf("Exiting the program.\n");
             exit(0);
         default:
-            printf("The inpute was incorrect and hence , Enter a new one: ");
+            printf("The input was incorrect; enter a new one.\n");
             break;
         }
     }
+
     return 0;
 }
