d4fec51bac58b19f1293937b92630a4af66e5389
Sarthak <ytsarthak1@gmail.com>
2025-12-01 18:51:28 +0545

    code

--- Java/src/App.java
+++ Java/src/App.java
@@ -1,5 +1,25 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Student obj = new Student("Sarthak", 12, 3.4);
+
+        obj.display();
+
+    }
+}
+
+class Student {
+
+    String name;
+    int age;
+    double gpa;
+
+    Student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + "  Age: " + age + "    Gpa: " + gpa);
     }
 }
