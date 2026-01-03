00b6d870321d07d1d0016a9425e2e6bb7193df6d
Sarthak <ytsarthak1@gmail.com>
2026-01-03 17:22:22 +0545

    calls

--- my_works/stack.c
+++ my_works/stack.c
@@ -1,98 +1,155 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct Node
 {
     int data;
     struct Node *next;
 };
 
-struct Node *last = NULL;
+struct Node *head = NULL;
 
+/* Insert at end */
+void Insert(int value)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = value;
+    ptr->next = NULL;
 
-
-void insert(int value) {
-    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-    newNode->data = value;
-    
-    if (last == NULL) {
-        newNode->next = newNode;
-        last = newNode;
-    } else {
-        newNode->next = last->next;
-        last->next = newNode;
-        last = newNode;
+    if (head == NULL)
+    {
+        head = ptr;
     }
-    printf("Inserted %d\n", value);
-}
-
-void delete() {
-    if (last == NULL) {
-        printf("Queue is empty!\n");
-        return;
-    }
-    struct Node *front = last->next;
-    if (front == last) {
-        printf("Deleted %d\n", front->data);
-        free(front);
-        last = NULL;
-    } else {
-        printf("Deleted %d\n", front->data);
-        last->next = front->next;
-        free(front);
-    }
-}
-
-void displayFront() {
-    if (last == NULL) {
-        printf("Queue is empty!\n");
-        return;
-    }
-    printf("Front element: %d\n", last->next->data);
-}
-
-void traverse() {
-    if (last == NULL) {
-        printf("Queue is empty!\n");
-        return;
-    }
-    struct Node *temp = last->next;
-    printf("Queue elements: ");
-    do {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    } while (temp != last->next);
-    printf("\n");
-}
-
-int main(void) {
-    int choice, value;
-    
-    while (1) {
-        printf("\n1. Insert\n2. Delete\n3. Display Front\n4. Traverse\n5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        
-        switch (choice) {
-            case 1:
-                printf("Enter value: ");
-                scanf("%d", &value);
-                insert(value);
-                break;
-            case 2:
-                delete();
-                break;
-            case 3:
-                displayFront();
-                break;
-            case 4:
-                traverse();
-                break;
-            case 5:
-                printf("Exiting...\n");
-                exit(0);
-            default:
-                printf("Invalid choice!\n");
+    else
+    {
+        struct Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
         }
+        temp->next = ptr;
     }
+
+    printf("Inserted %d into the list\n", value);
+}
+
+/* Delete a specific value */
+void Delete(int value)
+{
+    if (head == NULL)
+    {
+        printf("List is empty!\n");
+        return;
+    }
+
+    struct Node *temp = head;
+    struct Node *prev = NULL;
+
+    /* If head needs to be deleted */
+    if (temp->data == value)
+    {
+        head = temp->next;
+        free(temp);
+        printf("Deleted %d from the list\n", value);
+        return;
+    }
+
+    while (temp != NULL && temp->data != value)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+    {
+        printf("Value not found!\n");
+        return;
+    }
+
+    prev->next = temp->next;
+    free(temp);
+    printf("Deleted %d from the list\n", value);
+}
+
+/* Display first node */
+void displayFront()
+{
+    if (head == NULL)
+    {
+        printf("List is empty!\n");
+        return;
+    }
+
+    printf("First element: %d\n", head->data);
+}
+
+/* Traverse the list */
+void Traverse()
+{
+    if (head == NULL)
+    {
+        printf("List is empty!\n");
+        return;
+    }
+
+    struct Node *temp = head;
+    printf("Linked List: ");
+
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main()
+{
+    int choice, value;
+
+    printf("-----Menu-----\n");
+    printf("1. Insert\n");
+    printf("2. Delete\n");
+    printf("3. Display First\n");
+    printf("4. Traverse\n");
+    printf("5. Exit\n");
+
+    do
+    {
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter value to insert: ");
+            scanf("%d", &value);
+            Insert(value);
+            break;
+
+        case 2:
+            printf("Enter value to delete: ");
+            scanf("%d", &value);
+            Delete(value);
+            break;
+
+        case 3:
+            displayFront();
+            break;
+
+        case 4:
+            Traverse();
+            break;
+
+        case 5:
+            printf("Exiting...\n");
+            exit(0);
+
+        default:
+            printf("Invalid choice!\n");
+        }
+
+    } while (1);
+
     return 0;
 }
