1ce4c6301646085a91638ac237a7028e7e3a3a46
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-05-31 12:46:14 +0545

    Update Password Feature Added

--- my_works/PasswordManager.cpp
+++ my_works/PasswordManager.cpp
@@ -6,17 +6,19 @@ using namespace std;
 void showMenu()
 {
     cout << "\n -----Password Manager--------\n";
-    cout << "1.  Add Passoword : \n";
-    cout << "2. View Password: \n";
-    cout << "3. Search Password: \n";
-    cout << "4. Delete Password: \n";
-    cout << "5. Exit \n";
+    cout << "1. Add Password : \n";
+    cout << "2. View Password : \n";
+    cout << "3. Search Password : \n";
+    cout << "4. Delete Password : \n";
+    cout << "5. Update Password : \n";
+    cout << "6. Exit \n";
     cout << "Enter your choice: ";
 }
 
 void addPassword()
 {
     string website, username, password;
+
     cout << "Enter your Website: ";
     cin >> website;
 
@@ -32,15 +34,16 @@ void addPassword()
 
     file.close();
 
-    cout << "Password has been saved Successfully. \n";
+    cout << "Password has been saved Successfully.\n";
 }
 
 void viewPassword()
 {
     ifstream file("password.txt");
+
     if (!file)
     {
-        cout << "File can't be found and no password has been saved yet: \n";
+        cout << "File can't be found and no password has been saved yet.\n";
         return;
     }
 
@@ -52,6 +55,9 @@ void viewPassword()
     {
         int pos1 = line.find('|');
         int pos2 = line.rfind('|');
+
+        if (pos1 == string::npos || pos2 == string::npos)
+            continue;
 
         string website = line.substr(0, pos1);
         string username = line.substr(pos1 + 1, pos2 - pos1 - 1);
@@ -69,13 +75,15 @@ void viewPassword()
 void searchPassword()
 {
     string searchKey;
+
     cout << "Enter the website to search : ";
     cin >> searchKey;
 
     ifstream file("password.txt");
+
     if (!file)
     {
-        cout << "The file doesnt exist and no data is present: \n";
+        cout << "The file doesn't exist and no data is present.\n";
         return;
     }
 
@@ -87,24 +95,30 @@ void searchPassword()
         int pos1 = line.find('|');
         int pos2 = line.rfind('|');
 
+        if (pos1 == string::npos || pos2 == string::npos)
+            continue;
+
         string website = line.substr(0, pos1);
         string username = line.substr(pos1 + 1, pos2 - pos1 - 1);
         string password = line.substr(pos2 + 1);
 
         if (website == searchKey)
         {
-            cout << "=========Match Found==========\n";
+            cout << "========= Match Found ==========\n";
             cout << "Website : " << website << endl;
             cout << "Username : " << username << endl;
             cout << "Password : " << password << endl;
+
             found = true;
             break;
         }
     }
+
     file.close();
+
     if (!found)
     {
-        cout << "No password  was found: \n";
+        cout << "No password was found.\n";
     }
 }
 
@@ -120,7 +134,7 @@ void deletePassword()
 
     if (!file)
     {
-        cout << "The file doesn't exits \n";
+        cout << "The file doesn't exist.\n";
         return;
     }
 
@@ -133,7 +147,6 @@ void deletePassword()
         int pos2 = line.rfind('|');
 
         if (pos1 == string::npos || pos2 == string::npos)
-
             continue;
 
         string website = line.substr(0, pos1);
@@ -141,26 +154,85 @@ void deletePassword()
         if (website == searchKey)
         {
             found = true;
-            continue; // skip writing this line
+            continue;
         }
+
         temp << line << endl;
     }
 
     file.close();
     temp.close();
 
-    // replace old file
+    remove("password.txt");
+    rename("temp.txt", "password.txt");
+
+    if (found)
+        cout << "Password has been deleted successfully.\n";
+    else
+        cout << "Password wasn't found.\n";
+}
+
+void updatePassword()
+{
+    string searchKey;
+
+    cout << "Enter website name to update: ";
+    cin >> searchKey;
+
+    ifstream file("password.txt");
+    ofstream temp("temp.txt");
+
+    if (!file)
+    {
+        cout << "The file wasn't found.\n";
+        return;
+    }
+
+    string line;
+    bool found = false;
+
+    while (getline(file, line))
+    {
+        int pos1 = line.find('|');
+        int pos2 = line.rfind('|');
+
+        if (pos1 == string::npos || pos2 == string::npos)
+            continue;
+
+        string website = line.substr(0, pos1);
+        string username = line.substr(pos1 + 1, pos2 - pos1 - 1);
+        string password = line.substr(pos2 + 1);
+
+        if (website == searchKey)
+        {
+            string newPassword;
+
+            cout << "Enter the new password: ";
+            cin >> newPassword;
+
+            temp << website << "|"
+                 << username << "|"
+                 << newPassword << endl;
+
+            found = true;
+        }
+        else
+        {
+            temp << line << endl;
+        }
+    }
+
+    file.close();
+    temp.close();
 
     remove("password.txt");
     rename("temp.txt", "password.txt");
 
     if (found)
-        cout << "Password has been deleted sucessfully.\n";
+        cout << "The password has been updated successfully.\n";
     else
-        cout << "Password wasn't found. \n";
+        cout << "Website name not found.\n";
 }
-
-
 
 int main()
 {
@@ -190,14 +262,18 @@ int main()
             break;
 
         case 5:
-            cout << "Exiting program: \n";
+            updatePassword();
+            break;
+
+        case 6:
+            cout << "Exiting program.\n";
             break;
 
         default:
-            cout << "Invalid Choice: \n";
-            break;
+            cout << "Invalid Choice.\n";
         }
-    } while (choice != 5);
+
+    } while (choice != 6);
 
     return 0;
 }
