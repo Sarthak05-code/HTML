a2add3c84647b364093b13f3c5986083ad202e65
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-21 21:52:33 +0545

    comments. and stuffs

--- St_005/src/Abstract.java
+++ St_005/src/Abstract.java
@@ -1,25 +1,27 @@
 public class Abstract {
 
     public static void main(final String[] args) {
-        final Caller caller  = new Caller();
-        caller.runner();
-        caller.runner2();
-        caller.runner3();
+        StaticTester st = new StaticTester();
+        StaticTester.name();
+        st.caller();
     }
 }
 
 abstract class Main {
+
     abstract void runner();
 }
 
 interface MainInterface {
     public void runner2();
 }
+
 interface MainInterface2 {
     public void runner3();
 }
 
-class Caller extends Main implements MainInterface ,MainInterface2 {
+class Caller extends Main implements MainInterface, MainInterface2 {
+
     @Override
     public void runner() {
         System.out.println("The caller of the class. ");
@@ -32,6 +34,25 @@ class Caller extends Main implements MainInterface ,MainInterface2 {
 
     @Override
     public void runner3() {
-        System.out.println("This will run last. ");
+        System.out.println("This class will run last. ");
+    }
+}
+
+class StaticTester {
+
+    Caller caller = new Caller();
+
+    // this method call got turned into a method
+    // and will call the method of another class as well.
+    
+    public void caller() {
+        caller.runner();
+        caller.runner2();
+        caller.runner3();
+    }
+
+    // static method , the method here will be called directly by the className.
+    public static void name() {
+        System.out.println("The static caller. this is the static method");
     }
 }
