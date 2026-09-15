be9567c4e1269aa665c526d8318c31516c96bed0
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-09-15 17:38:25 +0545

    read and write

--- struct.c
+++ struct.c
@@ -3,11 +3,18 @@
 
 // File reader tester for the FileTester.txt file, read mode.
 int main(void) {
-  FILE *fptr = fopen("FileTester.txt", "r");
-  char c;
-  while (fscanf(fptr, "%c", &c)) {
-    printf("%c", c);
-    Sleep(10);
+  FILE *f = fopen("Sql.txt", "a");
+
+  if (f == NULL) {
+    printf("Error, opening the file.");
+    return 1; // exit program
   }
+
+  char input[100];
+  printf("Enter something to say here. \n");
+  fgets(input, sizeof(input), stdin);
+  fputs(input, f);
+  fclose(f);
+  printf("Written sucessfully.");
   return 0;
 }
